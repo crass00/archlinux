4634c570c98d9230050e5713e934e0774aef8c37
crass00 <crass00@hotmail.com>
2014-01-14 14:36:45 +0000

    more

--- comicopt/comicremovescene.sh
+++ comicopt/comicremovescene.sh
@@ -11,8 +11,55 @@ fi
 ORIGSIZE=`du -sb $1 | awk '{ print $1}'`
 
 # execute removal, this is a stupidly brute force way to do this
-
-
+Thumbs.db
+z.jpg
+xsou2.jpg
+xsou.jpg
+xSOU.jpg
+yyytiger.jpg
+zzztiger.jpg
+zzz-empire.jpg
+zzz empire.jpg
+zzz-nahga-empire.jpg
+zzzgfaceburn.jpg
+zzzgevilclown.jpg
+zz C9.jpg
+zzTLK.jpg
+zGJtag.jpg
+xxxTheManateeComplex.jpg
+zzz-DarthScanner-tag1.jpg
+zzz_stefcuk_tag.jpg
+wall_star-wars1.jpg
+zzz_TaruAd_Feb13HD.jpg
+zzzTaruAD July.jpg
+zzz_TaruAd_Jan13.jpg
+zzz_TaruAd_Mar13.jpg
+zzz_TaruAd_Jun13.jpg
+zzz_TaruAd_Jul13.jpg
+zzz_TaruAd_Aug13.jpg
+zzz_TaruAd_Sep13.jpg
+zzz_TaruAd_Oct13.jpg
+zzzBigRedGoat-Novus.jpg
+ZZ_Ath_Rosie&kingpin.jpg
+xxMMRosie and kingpin.jpg
+Z_MM_RK.jpg
+Z_RD_R.jpg
+ZKaty.jpg
+zwind.jpg
+zwater.jpg
+zWater.jpg
+ztatgirl.jpg
+zzz-BhM3-DCP.png
+zzz-BhM4b-DCP.png
+zGreenGiant.jpg
+zzz_whitewolf.jpg
+zTheRedStar.jpg
+zGGtag2.jpg
+zGGtag9.jpg
+zz_mm_emma.jpg
+zzz_minutemen_drunk with passion.jpg
+zzztag_the mother.jpg
+z-Just One Man.jpg
 
 # generate stats
 NEWSIZE=`du -sb $1 | awk '{ print $1}'`
